0b90da86f8e83f830b814362caba7546494e8849
Daniel Galion <daniel.galion94@gmail.com>
2019-04-24 00:56:57 +0200

    Added first screen layout.

--- app/src/androidTest/java/pl/lodz/uni/math/danielg/toothache/MainActivityInstrumentedTest.kt
+++
@@ -1,9 +0,0 @@
-package pl.lodz.uni.math.danielg.toothache
-
-import androidx.test.runner.AndroidJUnit4
-import org.junit.runner.RunWith
-
-@RunWith(AndroidJUnit4::class)
-class MainActivityInstrumentedTest {
-
-}
--- app/src/main/java/pl/lodz/uni/math/danielg/toothache/activities/MainActivity.kt
+++ app/src/main/java/pl/lodz/uni/math/danielg/toothache/activities/MainActivity.kt
@@ -2,9 +2,11 @@ package pl.lodz.uni.math.danielg.toothache.activities
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import pl.lodz.uni.math.danielg.toothache.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
     }
 }
