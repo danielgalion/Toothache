a2087fd42ad7db00400a65012fc5e372ca5646af
Daniel Galion <daniel.galion94@gmail.com>
2019-05-02 23:24:58 +0200

    Code refactored. Added TODO. @DentistDashboardActivity

--- app/src/main/java/pl/lodz/uni/math/danielg/toothache/activities/DentistDashboardActivity.kt
+++ app/src/main/java/pl/lodz/uni/math/danielg/toothache/activities/DentistDashboardActivity.kt
@@ -8,6 +8,7 @@ import android.view.Menu
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.GravityCompat
+import androidx.fragment.app.Fragment
 import com.google.android.material.navigation.NavigationView
 import kotlinx.android.synthetic.main.activity_dentist_dashboard.*
 import kotlinx.android.synthetic.main.navigation_view.*
@@ -38,30 +39,10 @@ class DentistDashboardActivity : AppCompatActivity(), NavigationView.OnNavigatio
     // TODO: Add office item. Or icon on the right hand side of a TopBar
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
         when (item.itemId) {
-            R.id.list -> {
-                Log.d(TAG, "List item is clicked. @onNavigationItemSelected(..)")
-                supportFragmentManager.beginTransaction()
-                        .replace(R.id.dentist_dashboard_fragment_lin_lay, DentistListFragment()).commit()
-                supportActionBar!!.title = "Lista"
-            }
-            R.id.edit -> {
-                Log.d(TAG, "Edit item is clicked. @onNavigationItemSelected(..)")
-                supportFragmentManager.beginTransaction()
-                        .replace(R.id.dentist_dashboard_fragment_lin_lay, DentistEditFragment()).commit()
-                supportActionBar!!.title = "Edytuj"
-            }
-            R.id.patient_dentist -> {
-                val intent = Intent(this, UsingTypeActivity::class.java)
-
-                // UsingTypeActivity is in launchMode "singleTask". So there won't be opened multiple windows of this activity.
-                UsingTypeSharedPreferencesManager.setUsingType(this, UsingTypeSharedPreferencesManager.USING_TYPE_NONE)
-                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK)
-                startActivity(intent)
-            }
-            R.id.sign_out -> {
-                UsingTypeSharedPreferencesManager.setUsingType(this, UsingTypeSharedPreferencesManager.USING_TYPE_NONE)
-                startActivity(Intent(this, DentistSignInActivity::class.java))
-            }
+            R.id.list -> NavigationViewHelper.onListItemClicked(this)
+            R.id.edit -> NavigationViewHelper.onEditItemClicked(this)
+            R.id.patient_dentist -> NavigationViewHelper.onPatientDentistItemClicked(this)
+            R.id.sign_out -> NavigationViewHelper.onSignOutItemClicked(this)
         }
 
         dentist_dashboard_drawer_lay_id.closeDrawer(GravityCompat.START)
@@ -77,14 +58,7 @@ class DentistDashboardActivity : AppCompatActivity(), NavigationView.OnNavigatio
 
     override fun onOptionsItemSelected(item: MenuItem?): Boolean {
         return when (item?.itemId) {
-            android.R.id.home -> {
-                if (dentist_dashboard_drawer_lay_id.isDrawerOpen(GravityCompat.START)) {
-                    dentist_dashboard_drawer_lay_id.closeDrawer(GravityCompat.START)
-                } else {
-                    dentist_dashboard_drawer_lay_id.openDrawer(GravityCompat.START)
-                }
-                true
-            }
+            android.R.id.home -> onDrawerSwitchClicked()
             R.id.dentist_dashboard_add_office_id -> {
                 startActivity(Intent(this, AddDentistOfficeActivity::class.java))
                 true
@@ -96,5 +70,51 @@ class DentistDashboardActivity : AppCompatActivity(), NavigationView.OnNavigatio
     private fun initNavView() {
         navigation_view_id.inflateMenu(R.menu.dentist_drawer_view)
         navigation_view_id.setNavigationItemSelectedListener(this)
+    }
+
+    private fun onDrawerSwitchClicked(): Boolean {
+        if (dentist_dashboard_drawer_lay_id.isDrawerOpen(GravityCompat.START)) {
+            dentist_dashboard_drawer_lay_id.closeDrawer(GravityCompat.START)
+        } else {
+            dentist_dashboard_drawer_lay_id.openDrawer(GravityCompat.START)
+        }
+        return true
+    }
+
+    // TODO: Read about Singleton and maybe use it for purpose of handling navigation items. But, I think it's quite OK now. At least it works.
+    //  Maybe search for a cleaner method with taking this out of this Activity class and to other file
+    //  (A package for every activity? – it sounds to complex for this project – long time to get the line of code).
+    //  But maybe I'm wrong. 'Cleaner' doesn't mean 'not complex'.
+    private object NavigationViewHelper {
+        fun onListItemClicked(activity: DentistDashboardActivity) {
+            Log.d(TAG, "List item is clicked. @onNavigationItemSelected(..)")
+            openFragment(activity, DentistListFragment())
+            activity.supportActionBar!!.title = "Lista"
+        }
+
+        fun onEditItemClicked(activity: DentistDashboardActivity) {
+            Log.d(TAG, "Edit item is clicked. @onNavigationItemSelected(..)")
+            openFragment(activity, DentistEditFragment())
+            activity.supportActionBar!!.title = "Edytuj"
+        }
+
+        fun onPatientDentistItemClicked(activity: DentistDashboardActivity) {
+            val intent = Intent(activity, UsingTypeActivity::class.java)
+
+            // UsingTypeActivity is in launchMode "singleTask". So there won't be opened multiple windows of this activity.
+            UsingTypeSharedPreferencesManager.setUsingType(activity, UsingTypeSharedPreferencesManager.USING_TYPE_NONE)
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK)
+            activity.startActivity(intent)
+        }
+
+        fun onSignOutItemClicked(activity: DentistDashboardActivity) {
+            UsingTypeSharedPreferencesManager.setUsingType(activity, UsingTypeSharedPreferencesManager.USING_TYPE_NONE)
+            activity.startActivity(Intent(activity, DentistSignInActivity::class.java))
+        }
+
+        private fun openFragment(activity: DentistDashboardActivity, fragment: Fragment) {
+            activity.supportFragmentManager.beginTransaction()
+                    .replace(R.id.dentist_dashboard_fragment_lin_lay, fragment).commit()
+        }
     }
 }
--- app/src/main/java/pl/lodz/uni/math/danielg/toothache/activities/UsingTypeActivity.kt
+++ app/src/main/java/pl/lodz/uni/math/danielg/toothache/activities/UsingTypeActivity.kt
@@ -2,7 +2,6 @@ package pl.lodz.uni.math.danielg.toothache.activities
 
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_using_type.*
