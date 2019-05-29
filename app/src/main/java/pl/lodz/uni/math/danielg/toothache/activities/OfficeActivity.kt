f21894b0eed17b615c515486d3f2558adc22502c
Daniel Galion <daniel.galion@cyrekdigital.com>
2019-05-29 16:29:41 +0200

    Added services RecView. Backup during trying to app a map properly.

--- app/src/main/java/pl/lodz/uni/math/danielg/toothache/activities/OfficeActivity.kt
+++ app/src/main/java/pl/lodz/uni/math/danielg/toothache/activities/OfficeActivity.kt
@@ -7,10 +7,14 @@ import androidx.core.view.ViewCompat
 import androidx.recyclerview.widget.DefaultItemAnimator
 import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.activity_office.*
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory
+import org.osmdroid.util.GeoPoint
 import pl.lodz.uni.math.danielg.toothache.R
+import pl.lodz.uni.math.danielg.toothache.adapters.OfficesDentalServicesAdapter
 import pl.lodz.uni.math.danielg.toothache.adapters.OfficesPhoneNumbersAdapter
 import pl.lodz.uni.math.danielg.toothache.managers.TopBarHelper
 import pl.lodz.uni.math.danielg.toothache.managers.toStringWithBrs
+import pl.lodz.uni.math.danielg.toothache.models.DentalService
 import pl.lodz.uni.math.danielg.toothache.models.Office
 
 class OfficeActivity : AppCompatActivity() {
@@ -24,6 +28,11 @@ class OfficeActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_office)
 
+        office_mapview_id.setTileSource(TileSourceFactory.DEFAULT_TILE_SOURCE)
+        office_mapview_id.setExpectedCenter(GeoPoint(51.8478115, 19.4195527))
+        office_mapview_id.minZoomLevel = 8.0
+        office_mapview_id.setUseDataConnection(true)
+
         onIntent()
         fillData()
         attachRecView()
@@ -32,12 +41,20 @@ class OfficeActivity : AppCompatActivity() {
 
     private fun attachRecView() {
         recycler_v_office_phone_numbers_id.layoutManager = LinearLayoutManager(this)
-        recycler_v_office_phone_numbers_id.adapter = OfficesPhoneNumbersAdapter(this, arrayListOf("+48 788 139 685"))
+        recycler_v_office_phone_numbers_id.adapter = OfficesPhoneNumbersAdapter(this, arrayListOf("+48 788 139 685", "+48 788 139 685", "+48 788 139 685"))
 
-        val animator = recycler_v_office_phone_numbers_id.itemAnimator as DefaultItemAnimator
+        recycler_v_office_dental_services_id.layoutManager = LinearLayoutManager(this)
+        recycler_v_office_dental_services_id.adapter = OfficesDentalServicesAdapter(this, arrayListOf(DentalService("Borowanie", 200), DentalService("Borowanie", 200), DentalService("Borowanie", 200), DentalService("Borowanie", 200), DentalService("Borowanie", 200)))
+
+        var animator = recycler_v_office_phone_numbers_id.itemAnimator as DefaultItemAnimator
 
         animator.supportsChangeAnimations = false
         ViewCompat.setNestedScrollingEnabled(recycler_v_office_phone_numbers_id, false)
+
+        animator = recycler_v_office_dental_services_id.itemAnimator as DefaultItemAnimator
+
+        animator.supportsChangeAnimations = false
+        ViewCompat.setNestedScrollingEnabled(recycler_v_office_dental_services_id, false)
     }
 
     override fun onOptionsItemSelected(item: MenuItem?): Boolean {
--- app/src/main/java/pl/lodz/uni/math/danielg/toothache/adapters/OfficesDentalServicesAdapter.kt
+++ app/src/main/java/pl/lodz/uni/math/danielg/toothache/adapters/OfficesDentalServicesAdapter.kt
@@ -4,25 +4,23 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
-import kotlinx.android.synthetic.main.rec_v_offices_phone_numbers_row.view.*
 import pl.lodz.uni.math.danielg.toothache.R
 import pl.lodz.uni.math.danielg.toothache.managers.CustomViewHolder
+import pl.lodz.uni.math.danielg.toothache.models.DentalService
 
-class OfficesPhoneNumbersAdapter(private val context: Context, private val phoneNumbers: ArrayList<String>) :
+class OfficesDentalServicesAdapter(private val context: Context, private val dentalServices: ArrayList<DentalService>) :
         RecyclerView.Adapter<CustomViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CustomViewHolder {
         val inflater = LayoutInflater.from(parent.context)
-        val row = inflater.inflate(R.layout.rec_v_offices_phone_numbers_row, parent, false)
+        val row = inflater.inflate(R.layout.rec_v_offices_dental_services_row, parent, false)
 
-        return CustomViewHolder(row, row.rec_v_office_phone_numbers_call_button_id)
+        return CustomViewHolder(row)
     }
 
     override fun getItemCount(): Int {
-        return phoneNumbers.size
+        return dentalServices.size
     }
-
-//    TODO: Change hardcoded phone number.
 
     override fun onBindViewHolder(holder: CustomViewHolder, position: Int) {
 
