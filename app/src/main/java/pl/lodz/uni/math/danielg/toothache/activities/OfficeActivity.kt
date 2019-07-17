c54e116c9a5ee12a41bdceafbdde82bf3583af56
Daniel Galion <daniel.galion@cyrekdigital.com>
2019-07-17 14:19:04 +0200

    Added map that displays properly.

--- app/src/main/java/pl/lodz/uni/math/danielg/toothache/activities/OfficeActivity.kt
+++ app/src/main/java/pl/lodz/uni/math/danielg/toothache/activities/OfficeActivity.kt
@@ -1,12 +1,14 @@
 package pl.lodz.uni.math.danielg.toothache.activities
 
 import android.os.Bundle
+import android.preference.PreferenceManager
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.recyclerview.widget.DefaultItemAnimator
 import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.activity_office.*
+import org.osmdroid.config.Configuration
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory
 import org.osmdroid.util.GeoPoint
 import pl.lodz.uni.math.danielg.toothache.R
@@ -26,35 +28,17 @@ class OfficeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // OpenStreetMap configuration:
+        Configuration.getInstance().load(applicationContext, PreferenceManager.getDefaultSharedPreferences(applicationContext))
+
         setContentView(R.layout.activity_office)
 
-//        office_mapview_id.setTileSource(TileSourceFactory.DEFAULT_TILE_SOURCE)
-//        office_mapview_id.setExpectedCenter(GeoPoint(51.8478115, 19.4195527))
-//        office_mapview_id.minZoomLevel = 8.0
-//        office_mapview_id.setUseDataConnection(true)
-
+        setupMap()
         onIntent()
         fillData()
         attachRecView()
         TopBarHelper.setUp(this, "Gabinet", true)
-    }
-
-    private fun attachRecView() {
-        recycler_v_office_phone_numbers_id.layoutManager = LinearLayoutManager(this)
-        recycler_v_office_phone_numbers_id.adapter = OfficesPhoneNumbersAdapter(this, arrayListOf("+48 788 139 685", "+48 788 139 685", "+48 788 139 685"))
-
-        recycler_v_office_dental_services_id.layoutManager = LinearLayoutManager(this)
-        recycler_v_office_dental_services_id.adapter = OfficesDentalServicesAdapter(this, arrayListOf(DentalService("Borowanie", 200), DentalService("Borowanie", 200), DentalService("Borowanie", 200), DentalService("Borowanie", 200), DentalService("Borowanie", 200)))
-
-        var animator = recycler_v_office_phone_numbers_id.itemAnimator as DefaultItemAnimator
-
-        animator.supportsChangeAnimations = false
-        ViewCompat.setNestedScrollingEnabled(recycler_v_office_phone_numbers_id, false)
-
-        animator = recycler_v_office_dental_services_id.itemAnimator as DefaultItemAnimator
-
-        animator.supportsChangeAnimations = false
-        ViewCompat.setNestedScrollingEnabled(recycler_v_office_dental_services_id, false)
     }
 
     override fun onOptionsItemSelected(item: MenuItem?): Boolean {
@@ -65,6 +49,16 @@ class OfficeActivity : AppCompatActivity() {
             }
             else              -> super.onOptionsItemSelected(item)
         }
+    }
+
+    private fun setupMap() {
+        office_mapview_id.setTileSource(TileSourceFactory.MAPNIK)
+//        office_mapview_id.setExpectedCenter(GeoPoint(51.8478115, 19.4195527))
+//        office_mapview_id.minZoomLevel = 8.0
+        office_mapview_id.setMultiTouchControls(false)
+        office_mapview_id.setUseDataConnection(true)
+        office_mapview_id.controller.setCenter(GeoPoint(51.8478115, 19.4195527))
+        office_mapview_id.controller.setZoom(17.0)
     }
 
     private fun onIntent() {
@@ -78,5 +72,33 @@ class OfficeActivity : AppCompatActivity() {
 
         office_name_txt_v_id.text = office?.name
         office_dr_names_txt_v_id.text = office?.doctorsNames?.toStringWithBrs()
+    }
+
+    private fun attachRecView() {
+        recycler_v_office_phone_numbers_id.layoutManager = LinearLayoutManager(this)
+        recycler_v_office_phone_numbers_id.adapter =
+                OfficesPhoneNumbersAdapter(this, arrayListOf("+48 788 139 685", "+48 788 139 685", "+48 788 139 685"))
+
+        recycler_v_office_dental_services_id.layoutManager = LinearLayoutManager(this)
+        recycler_v_office_dental_services_id.adapter = OfficesDentalServicesAdapter(
+                this,
+                arrayListOf(
+                        DentalService("Borowanie", 200),
+                        DentalService("Borowanie", 200),
+                        DentalService("Borowanie", 200),
+                        DentalService("Borowanie", 200),
+                        DentalService("Borowanie", 200)
+                )
+        )
+
+        var animator = recycler_v_office_phone_numbers_id.itemAnimator as DefaultItemAnimator
+
+        animator.supportsChangeAnimations = false
+        ViewCompat.setNestedScrollingEnabled(recycler_v_office_phone_numbers_id, false)
+
+        animator = recycler_v_office_dental_services_id.itemAnimator as DefaultItemAnimator
+
+        animator.supportsChangeAnimations = false
+        ViewCompat.setNestedScrollingEnabled(recycler_v_office_dental_services_id, false)
     }
 }
